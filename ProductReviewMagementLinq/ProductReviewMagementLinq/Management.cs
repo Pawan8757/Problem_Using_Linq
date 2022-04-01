d9e72f2cb1ad35d56b85d47ab1d6cab38bad48af
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-04-01 06:04:22 +0530

    adding product management using Linq concept

---
+++ ProductReviewMagementLinq/ProductReviewMagementLinq/Management.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ProductReviewMagementLinq
+{
+   public class Management
+    {
+        //UC-2
+        public void TopRecordsList(List<ProductReview> review)
+        {
+            var recordData = (from productreviews in review
+                              orderby productreviews.Rating descending
+                              select productreviews).Take(3);
+            foreach (var list in recordData)
+            {
+                Console.WriteLine("ProductId :" +list.ProductId+ "UserId :" +list.UserId + "Rating :" +list.Rating + "Review :" +list.Review + "IsLike :" +list.IsLike);
+
+            }
+        }
+
+        //UC-3
+        public void SelectRecordsList(List<ProductReview> review)
+        {
+            var recordData = (from productreviews in review
+                              where (productreviews.ProductId == 1 && productreviews.Rating > 3)
+                              || (productreviews.ProductId == 4 && productreviews.Rating > 3)
+                              || (productreviews.ProductId == 9 && productreviews.Rating > 3)
+                              select productreviews);
+
+            foreach (var list in recordData)
+            {
+                Console.WriteLine("ProductId :" + list.ProductId + "UserId :" + list.UserId + "Rating :" + list.Rating + "Review :" + list.Review + "IsLike :" + list.IsLike);
+
+            }
+        }
+        //UC-4
+        public void CountOfReviews(List<ProductReview> review)
+        {
+            var recordData = review.GroupBy(x => x.ProductId).Select(x => new { ProductId = x.Key, Count = x.Count ()});
+
+            foreach (var list in recordData)
+            {
+                Console.WriteLine("ProductId :" + list.ProductId + "Count :" +list.Count );
+
+            }
+        }
+    }
+}
