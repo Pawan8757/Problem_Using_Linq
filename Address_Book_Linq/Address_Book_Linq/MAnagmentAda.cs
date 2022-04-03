126470d8b9120e49c7375a51b928fd58b540d6c5
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-04-03 16:21:23 +0530

    adding Address book solution using linq

---
+++ Address_Book_Linq/Address_Book_Linq/MAnagmentAda.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookLinq
+{
+    public class MAnagmentAda
+    {
+
+
+
+
+        //UC6
+
+        public void Retrive(List<Address> listpe)
+        {
+            var RecordData3 = listpe.GroupBy(x => x.city).Select(x => new { city = x.Key });
+
+            foreach (var list in RecordData3)
+            {
+                Console.WriteLine("City:" + list.city);
+            }
+        }
+
+        //UC7
+        public void Size(List<Address> listpe)
+        {
+            var RecordData = listpe.GroupBy(x => x.city).Select(x => new { city = x.Key, Count = x.Count() });
+
+
+
+            foreach (var list in RecordData)
+            {
+                Console.WriteLine("Product id:" + list.city + "Count" + list.Count);
+            }
+
+
+        }
+
+        //UC8
+
+        public void Alpha(List<Address> listpe)
+        {
+            var RecordData = (from adresreviews in listpe orderby adresreviews.empFirstName descending select adresreviews);
+
+
+            foreach (var list in RecordData)
+            {
+                Console.WriteLine("First Nmae" + list.empFirstName + "City" + list.city);
+            }
+        }
+        //UC10
+
+        public void RetriveContact(List<Address> listpe)
+        {
+            var RecordData9 = listpe.GroupBy(x => x.contact).Select(x => new { contact = x.Key });
+
+            foreach (var list in RecordData9)
+            {
+                Console.WriteLine("Contact:" + list.contact);
+            }
+        }
+
+
+    }
+}
---
+++ Address_Book_Linq/Address_Book_Linq/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace AddressBookLinq;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Address> listperson = new List<Address>()
+        {
+            new Address() { EmpId = 1, empFirstName = "Pawan", empLastName = "Yadav", Adress = "Gopal NAgar Durga MAndir", city = "Asansol", state="West Bengal", Zip="713340", email="niravpandit0002@gmail.com" },
+            new Address() { EmpId = 2, empFirstName = "Premwa", empLastName = "Giri", Adress = "Claifornia 223", city = "Calfornia", state = "Continant", Zip = "713340", email = "David johon@gmail.com" },
+            new Address() { EmpId = 3, empFirstName = "Anand", empLastName = "Priyadarshi", Adress = "kaloplis", city = "PDosidon", state = "Temple of Zeus", Zip = "713340", email = "Kassandra0002@gmail.com" },
+            new Address() { EmpId = 4, empFirstName = "Piyush", empLastName = "varshney", Adress = "Giza", city = "Egypt", state = "mishr", Zip = "713340", email = "BAyek@gmail.com" }
+
+
+        };
+
+        foreach (var list in listperson)
+        {
+            Console.WriteLine("EmpId" + list.EmpId + "empFirstName:" + list.empFirstName);
+        }
+
+        //UC3 Adding New Contacts
+
+        listperson.Add(new Address() { EmpId = 1, empFirstName = "Vis", empLastName = "akaha", Adress = "Gopal NAgar Durga MAndir", city = "Asansol", state = "West Bengal", Zip = "713340", email = "niravpandit0002@gmail.com", contact = 775667 });
+        listperson.Add(new Address() { EmpId = 1, empFirstName = "Vishal", empLastName = "Dute", Adress = "", city = "Mahara", state = "Maharasrtra", Zip = "2213", email = "vishalDute@gmail.com", contact = 775667 });
+
+        foreach (var list in listperson)
+        {
+            Console.WriteLine("EmpId" + list.EmpId + "empFirstName:" + list.empFirstName + "LatName" + list.empLastName);
+        }
+
+
+
+        //UC5
+        //listperson.Remove(new Adress() { EmpId = 1, empFirstName = "Vis", empLastName = "akaha", Address = "Gopal NAgar Durga MAndir", city = "Asansol", state = "West Bengal", Zip = "713340", email = "niravpandit0002@gmail.com", contact = 775667 });
+
+
+        MAnagmentAda m = new MAnagmentAda();
+
+        m.Retrive(listperson);
+        m.Size(listperson);
+        m.Alpha(listperson);
+        m.RetriveContact(listperson);
+
+
+    }
+}
